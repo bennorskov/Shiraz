fb34edbabe8d7da3ac635e0b4f29485506469208
Ben Norskov <ben@playistheantidote.com>
2016-07-17 14:49:24 -0400

    Initial Commit of first prototype

---
+++ Assets/buildingSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buildingSpawner : MonoBehaviour {
+
+	private GameObject building;
+	public int buildingSequenceNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+		GlobalGeneratorValues.initBuildingPrefabs();
+		building = GlobalGeneratorValues.getRandomBuilding();
+		building.transform.localScale = Vector3.one * GlobalGeneratorValues.fullSizeBuildingScale *  Mathf.Pow( GlobalGeneratorValues.buildingReduction, buildingSequenceNumber);
+		Instantiate(building, transform.position, transform.rotation);
+	}
+}
---
+++ Assets/playerMover.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMover : MonoBehaviour {
+
+	public float acceleration = 2f;
+
+	private Rigidbody rb;
+
+	// Building spawner variables
+	private float amountToMoveBeforeSpawningABuilding = 100f;
+	private Vector3 lastSpawnPosition;
+	private float spawnRangeForNewBuildingSpawner;
+	private GameObject buildingSpawner;
+
+	// Use this for initialization
+	void Start () {
+		GlobalGeneratorValues.initBuildingPrefabs(); // this needs to be called somewhere; there's only one player
+
+		rb = GetComponent<Rigidbody>();
+		lastSpawnPosition = transform.position;
+		buildingSpawner = (GameObject) Resources.Load("buildingBuilder");
+		spawnRangeForNewBuildingSpawner = Camera.main.farClipPlane;
+	}
+
+	void FixedUpdate () {
+		rb.AddForce( transform.forward * acceleration);
+	}
+
+	void Update() {
+		if (Vector3.Distance( transform.position, lastSpawnPosition) > amountToMoveBeforeSpawningABuilding ) {
+			// create a building spawner at spawn range in front of you
+			print("new building!");
+			Instantiate(buildingSpawner, transform.forward.normalized * spawnRangeForNewBuildingSpawner, GlobalGeneratorValues.getSpawnRotation());
+			lastSpawnPosition = transform.position;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawLine(transform.position, transform.forward.normalized * spawnRangeForNewBuildingSpawner);
+	}
+}
